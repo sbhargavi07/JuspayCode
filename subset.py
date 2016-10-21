109f933b0c13c3d2c8219b36f60951deede998c8
Bhargavi Sri <sribhargavi07@gmail.com>
2016-10-22 01:33:55 +0530

    Update subset.py

--- subset.py
+++ subset.py
@@ -1,3 +1,4 @@
+import sys
 def isSubset(list1,list2):
     for val in list1:
         if val in list2:
@@ -6,16 +7,18 @@ def isSubset(list1,list2):
             print("False")
             sys.exit()
     print("True")
-
-list1 = []
-n = int(input())
-list2 = []
-m = int(input())
-tempVar = 0
-for tempVar in range(n):
-    temp = input()
-    list1.append(temp)
-for tempVar in range(m):
-    temp = input()
-    list2.append(temp)
-isSubset(list1,list2)
+try:
+    list1 = []
+    n = int(input())
+    list2 = []
+    m = int(input())
+    tempVar = 0
+    for tempVar in range(n):
+        temp = input()
+        list1.append(temp)
+    for tempVar in range(m):
+        temp = input()
+        list2.append(temp)
+    isSubset(list1,list2)
+except:
+    pass
